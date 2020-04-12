56e3f1f39dc02dd66377232a9c778cfb622208d4
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-12 10:04:26 +0300

    Created low level Switchable class - Fan

---
+++ src/main/java/com/example/dependencyinversionprinciple/lowlevel/Fan.java
@@ -0,0 +1,16 @@
+package com.example.dependencyinversionprinciple.lowlevel;
+
+import com.example.dependencyinversionprinciple.highlevel.Switchable;
+
+public class Fan implements Switchable {
+    @Override
+    public void turnOn() {
+        System.out.println("Fan: Fan turned on...");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Fan: Fan turned off...");
+
+    }
+}
