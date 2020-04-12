507708198cf7d3b0ac21744f06598e905b2d4347
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-12 09:40:33 +0300

    Initial commit

---
+++ src/main/java/com/example/dependencyinversionprinciple/DependencyInversionPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinversionprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInversionPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInversionPrincipleApplication.class, args);
+	}
+
+}
