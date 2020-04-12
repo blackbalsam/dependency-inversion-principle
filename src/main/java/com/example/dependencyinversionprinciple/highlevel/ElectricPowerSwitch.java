c98575d1cad1d7af70678136f707b85f9da89a42
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-12 10:03:45 +0300

    Created high level implementation of Switch interface and referred to Switchable Interface instead of any concrete class

---
+++ src/main/java/com/example/dependencyinversionprinciple/highlevel/ElectricPowerSwitch.java
@@ -0,0 +1,26 @@
+package com.example.dependencyinversionprinciple.highlevel;
+
+public class ElectricPowerSwitch implements Switch {
+    public Switchable client;
+    public boolean on;
+    public ElectricPowerSwitch(Switchable client) {
+        this.client = client;
+        this.on = false;
+    }
+
+    @Override
+    public boolean isOn() {
+        return this.on;
+    }
+
+    @Override
+    public void press() {
+        if (isOn()) {
+            client.turnOff();
+            this.on = false;
+        } else {
+            client.turnOn();
+            this.on = true;
+        }
+    }
+}
